71aeee0e4a06e87adcdcd3a470c02328c49ea19c
Ross Greenwood <rossngreenwood@gmail.com>
2017-10-16 11:44:33 -0700

    Fixed ODE solver with Jacobian and events

--- Code/compare_instantons.m
+++ Code/compare_instantons.m
@@ -7,7 +7,7 @@ phi_absMin  = 3;
 
 xguess           = 2;   
 xtol             = 1e-4; 
-phitol           = 1e-4; 
+phitol           = 1e-4;
 thinCutoff       = 1e-2; 
 npoints          = 500;  
 rmin             = 1e-5; 
@@ -17,24 +17,21 @@ max_interior_pts = 0;
 % profile on
 
 % Initialize object
-tic
 fvi = FalseVacuumInstanton(...
     'V',@(x) (x.^4 - 14*x.^2 - 24*x)*1e-0,...
     'dV',@(x) (4*x.^3 - 28*x - 24)*1e-0,...
     'd2V',@(x) (12*x.^2 - 28)*1e-0,...
-    'M_Pl',sqrt(8*pi).^3,...
+    'M_Pl',sqrt(8*pi),...
     'phi_metaMin',phi_metaMin,...
     'phi_absMin',phi_absMin);
 
+tic
 % Get profile
-[R,Y,~,n_interior_pts] = fvi.find_profile(xguess,xtol,phitol,thinCutoff,npoints,rmin,rmax,max_interior_pts);
-
-% Calculate Euclidean action
-S = fvi.find_action(R,Y);
-lambda = fvi.find_tunneling_rate(R,Y);
-
+[R,Y] = fvi.find_profile(xguess,xtol,phitol,thinCutoff,npoints,rmin,rmax);
 toc
 
-plot(R,Y(1,:))
+% Calculate Euclidean action
+lambda = fvi.find_tunneling_rate(R,Y);
+B = -log(lambda);
 
-% profile viewer
+plot(R,Y(1,:))

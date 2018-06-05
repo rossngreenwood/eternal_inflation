dba8b0b4528ea37c1a786bc96e75a7b5894ef901
Ross Greenwood <rossngreenwood@gmail.com>
2018-06-04 20:22:24 -0700

    Restored eternal_sim_leader.m

--- code/eternal_sim_leader.m
+++ code/eternal_sim_leader.m
@@ -1,11 +1,19 @@
-function eternal_sim_leader(cores,input_file,output_dir)
+
+function eternal_sim_leader(cores,flag_range,range,input_file,output_file,output_dir)
     
-    if nargin < 2, input_file = 'infile.txt';   end
-    if nargin < 3, output_dir = '';             end
+    warning('off');
+    
+    if nargin < 4, input_file = 'infile.txt';   end
+    if nargin < 5, output_file = 'outfile.txt'; end
+    if nargin < 6, output_dir = '';             end
+    
+    parpool(cores) % Open a parallel pool
+    
+    for test_id = range(1):range(2)
     
     %% Gather parameters from input file
     
-    fid = fopen(input_file,'r');
+    fid = fopen(sprintf(input_file,test_id),'r');
     meta_line = fgets(fid);
     is = 1; fclose(fid);
     
@@ -14,12 +22,12 @@ function eternal_sim_leader(cores,input_file,output_dir)
     [mh,~,~,is1]     = sscanf(meta_line(is:end),'%G,',1); is = is+is1-1;
     [kmax,~,~,is1]   = sscanf(meta_line(is:end),'%d,',1); is = is+is1-1;
     [gamma,~,~,is1]  = sscanf(meta_line(is:end),'%f,',1); is = is+is1-1;
-    
+
     [measure,~,~,is1]           = sscanf(meta_line(is:end),'%c,',1); is = is+is1-1;
     [n_tunnel_max,~,~,is1]      = sscanf(meta_line(is:end),'%d,',1); is = is+is1-1;
     [lambdascreen,~,~,is1]      = sscanf(meta_line(is:end),'%d,',1); is = is+is1-1;
-    [mv_offset_max,~,~,is1]     = sscanf(meta_line(is:end),'%G,',1); is = is+is1-1;
-    [auxFlag,~,~,is1]           = sscanf(meta_line(is:end),'%d,',1); is = is+is1-1;
+    [rho_Lambda_thres,~,~,is1]  = sscanf(meta_line(is:end),'%G,',1); is = is+is1-1;
+    [fixQ,~,~,is1]              = sscanf(meta_line(is:end),'%d,',1); is = is+is1-1;
     [Nafter,~,~,is1]            = sscanf(meta_line(is:end),'%G,',1); is = is+is1-1;
     [seed,~,~,is1]              = sscanf(meta_line(is:end),'%d,',1); is = is+is1-1;
     [n_recycle,~,~,is1]         = sscanf(meta_line(is:end),'%d,',1); is = is+is1-1;
@@ -32,12 +40,12 @@ function eternal_sim_leader(cores,input_file,output_dir)
     
     %% Run code in parallel
     
-    parpool(cores) % Open a parallel pool
     
     spmd % Execute this code in parallel
         
-        worker_outfile = strcat(output_dir,sprintf('.worker_%d.txt',labindex-1));
-        
+        worker_outfile = strcat(...
+            sprintf(output_dir,test_id),sprintf('.worker_%d.txt',labindex-1));
+        disp(worker_outfile)
         warning('off');
         
         eis = EternalInflationSimulator(...
@@ -50,7 +58,7 @@ function eternal_sim_leader(cores,input_file,output_dir)
             'measure',          measure,...
             'n_tunnel_max',     n_tunnel_max,...
             'lambdascreenmode', logical(lambdascreen),...
-            'mv_offset_max',    mv_offset_max,...
+            'mv_offset_max', rho_Lambda_thres,...
             'Nafter',           Nafter,...
             'n_recycle',        n_recycle,...
             'randstream',       rs{labindex},...
@@ -59,5 +67,9 @@ function eternal_sim_leader(cores,input_file,output_dir)
         eis.main();
         
     end
+    
+    end % for
+
+    delete(gcp('nocreate'));
     
 end
--- code/slurm_select.sh
+++ code/slurm_select.sh
@@ -6,12 +6,12 @@ if [[ ${#OUTPUT} == "0" ]]; then
   args+="44 "
   args+="$@"
   comment+="$@"
-  sbatch --partition 256x44 --cpus-per-task 44 /hb/home/rngreenw/eternal_inflation/code/slurm_matlab2.sh $args
+  sbatch --partition 256x44 --cpus-per-task 44 /hb/home/rngreenw/eternal_inflation/code/slurm_matlab.sh $args
 else
   args+="24 "
   args+="$@"
   comment+="\""
   comment+="$@"
   comment+="\""
-  sbatch --partition 128x24 --cpus-per-task 24 /hb/home/rngreenw/eternal_inflation/code/slurm_matlab2.sh $args
+  sbatch --partition 128x24 --cpus-per-task 24 /hb/home/rngreenw/eternal_inflation/code/slurm_matlab.sh $args
 fi

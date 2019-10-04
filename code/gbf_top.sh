4d9a889588d2e121ab097754f245b2a6ac234460
Ross Greenwood <rossngreenwood@gmail.com>
2019-10-04 11:31:59 -0700

    Restored gbf_top

--- code/gbf_top.sh
+++ code/gbf_top.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=truncgbf
-#SBATCH --ntasks=1
+#SBATCH --ntasks=5
 #SBATCH --mem-per-cpu=100
 #SBATCH --time=8:00:00
 #SBATCH --output=/hb/home/rngreenw/eternal_inflation/data/truncgbf_%j.out
@@ -9,10 +9,31 @@
 #SBATCH --mail-type=end
 #SBATCH --mail-user=rossngreenwood@gmail.com
 
-run_ids =
-weights =
+run_ids = "np.arange(115,1216+1)"
+weights = "np.ones((1,1216-115+1)"
 
-cmd = "import numpy as np; import pandas as pd; from jup_funs import massBin2D; massBin2D(run_ids="$run_ids",weights="$weights",cutparam=['n_s','r'],cutbounds=[[0.95,0.975],[0,0.064]],outfile='fractions_310.csv'); exit()"
+sh gen_output_t.sh 115 1216 "[record_flag]" "[[3]]" 
+
+ind = 315
+
+cmd = "import numpy as np; from jup_funs import massBin2D; fractions=massBin2D(run_ids="$run_ids"); fractions.to_csv('~/eternal_inflation/data/fractions_"$ind".csv'); exit()"
 srun python3 -c $cmd
+((ind++))
+
+cmd = "import numpy as np; from jup_funs import massBin2D; fractions=massBin2D(run_ids="$run_ids",cutParams=['n_s'],cutBounds=[[0.95,0.975]]); fractions.to_csv('~/eternal_inflation/data/fractions_"$ind".csv'); exit()"
+srun python3 -c $cmd
+((ind++))
+
+cmd = "import numpy as np; from jup_funs import massBin2D; fractions=massBin2D(run_ids="$run_ids",cutParams=['r'],cutBounds=[[0,0.064]]); fractions.to_csv('~/eternal_inflation/data/fractions_"$ind".csv'); exit()"
+srun python3 -c $cmd
+((ind++))
+
+cmd = "import numpy as np; from jup_funs import massBin2D; fractions=massBin2D(run_ids="$run_ids",cutParams=['n_s','r'],cutBounds=[[0.95,0.975],[0,0.064]]); fractions.to_csv('~/eternal_inflation/data/fractions_"$ind".csv'); exit()"
+srun python3 -c $cmd
+((ind++))
+
+cmd = "import numpy as np; from jup_funs import massBin2D; fractions=massBin2D(run_ids="$run_ids",cutParams=['Q'],cutBounds=[[np.power(10.,-4.5),np.power(10.,-4.1)]]); fractions.to_csv('~/eternal_inflation/data/fractions_"$ind".csv'); exit()"
+srun python3 -c $cmd
+((ind++))
 
 wait
